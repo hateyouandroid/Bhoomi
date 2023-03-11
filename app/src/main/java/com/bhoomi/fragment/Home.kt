c72ef5d8954e4de52dbdaaf8282a7744a2a0a236
vishal <vishalvidhate196@gmail.com>
2023-03-11 16:43:21 +0530

    added home_1 new button

--- app/src/main/java/com/bhoomi/fragment/Home.kt
+++ app/src/main/java/com/bhoomi/fragment/Home.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.bhoomi.R
 
 
@@ -12,10 +14,16 @@ class Home : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
