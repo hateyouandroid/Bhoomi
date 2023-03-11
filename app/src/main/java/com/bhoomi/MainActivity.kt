e903110b0b2bf659852f933515a4da2bfd0b599e
vishal <vishalvidhate196@gmail.com>
2023-03-11 23:22:30 +0530

    change Home button

--- app/src/main/java/com/bhoomi/MainActivity.kt
+++ app/src/main/java/com/bhoomi/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bhoomi
 
+import android.content.Intent
 import com.bhoomi.base.BaseActivity
 import com.bhoomi.databinding.ActivityMainBinding
 import com.bhoomi.ext.printTost
@@ -10,7 +11,10 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onClick() {
 
-
+        binding.notifiy.setOnClickListener {
+            val intent = Intent(this,NotificationActivity::class.java)
+            startActivity(intent)
+        }
     }
     override fun initView() {
         super.initView()
--- app/src/main/java/com/bhoomi/NotificationActivity.kt
+++ app/src/main/java/com/bhoomi/NotificationActivity.kt
@@ -2,10 +2,18 @@ package com.bhoomi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bhoomi.base.BaseActivity
+import com.bhoomi.databinding.ActivityNotificationBinding
+import com.bhoomi.ext.startActivity
 
-class NotificationActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
+class NotificationActivity: BaseActivity<ActivityNotificationBinding>(ActivityNotificationBinding::inflate) {
+
+    override fun onClick() {
+        binding.imgClose.setOnClickListener {
+           startActivity(MainActivity())
+        }
+    }
+    override fun initView() {
+        super.initView()
     }
 }
